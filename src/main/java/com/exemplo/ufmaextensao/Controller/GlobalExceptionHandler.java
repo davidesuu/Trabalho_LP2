0fd6774f0f40a2273da91662a7c7c510d4d4b99e
davidesuu <davoidsmartins@gmail.com>
2026-06-19 01:05:32 -0300

    feat: Add global exception handler

---
+++ src/main/java/com/exemplo/ufmaextensao/Controller/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.exemplo.ufmaextensao.Controller;
+
+import com.exemplo.ufmaextensao.service.RegraDeNegocioException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RegraDeNegocioException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleRegraDeNegocio(RegraDeNegocioException e) {
+        return e.getMessage();
+    }
+
+}
