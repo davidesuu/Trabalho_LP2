dd62990d97ff436a17f73ff954fe2aea6501bd20
davidesuu <davoidsmartins@gmail.com>
2026-06-20 01:19:21 -0300

    feat(DTO): adiciona DocenteDTO

---
+++ src/main/java/com/exemplo/ufmaextensao/DTO/DocenteDTO.java
@@ -0,0 +1,12 @@
+package com.exemplo.ufmaextensao.DTO;
+
+import lombok.Data;
+
+@Data
+public class DocenteDTO {
+    private String nome;
+    private String email;
+    private String senha;
+    private String siape;
+    private String departamento;
+}
