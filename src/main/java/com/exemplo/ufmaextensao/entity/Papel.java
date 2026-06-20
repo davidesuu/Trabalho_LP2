967f3af17bb576c47e63ea752fc26492e5af5735
davidesuu <davoidsmartins@gmail.com>
2026-06-20 00:43:38 -0300

    feat(dto): adiciona DiscenteDTO e PPCDTO

--- src/main/java/com/exemplo/ufmaextensao/entity/Papel.java
+++ src/main/java/com/exemplo/ufmaextensao/entity/Papel.java
@@ -1,6 +1,8 @@
 package com.exemplo.ufmaextensao.entity;
 
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -15,6 +17,7 @@ public class Papel {
 
     private String nome; // "COORDENADOR", "ADMIN", "DOCENTE"
 
+    @JsonIgnore
     @ManyToMany(mappedBy = "papeis")
     private List<Usuario> usuarios;
 }
