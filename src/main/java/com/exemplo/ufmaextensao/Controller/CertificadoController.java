4e76e7f9e817007aeecf532e8c5ba25e8a83921c
davidesuu <davoidsmartins@gmail.com>
2026-06-28 22:18:14 -0300

    fix: Corrigindo certificadoController

--- src/main/java/com/exemplo/ufmaextensao/Controller/CertificadoController.java
+++ src/main/java/com/exemplo/ufmaextensao/Controller/CertificadoController.java
@@ -23,7 +23,7 @@ public class CertificadoController {
             @RequestParam Integer id_discente,
             @RequestParam Integer id_docente,
             @RequestParam Integer id_oportunidade,
-            @RequestBody CertificadoDTO certificadoDTO) {
+            @RequestBody CertificadoDTO certificadoDTO) throws RegraDeNegocioException{
         try{
             Certificado novoCertificado = certificadoService.criarCertificado(id_discente, id_docente, certificadoDTO, id_oportunidade);
             return  ResponseEntity.status(HttpStatus.CREATED).body(novoCertificado);
@@ -35,7 +35,7 @@ public class CertificadoController {
     @PutMapping("/assinar")
     public ResponseEntity<String> assinar(
             @RequestParam Integer id_docente,
-            @RequestBody CertificadoDTO certificadoDTO){
+            @RequestBody CertificadoDTO certificadoDTO) throws RegraDeNegocioException{
         try {
             certificadoService.assinarCertificado(certificadoDTO, id_docente);
             return ResponseEntity.ok("certificado assinado com sucesso");
@@ -45,19 +45,19 @@ public class CertificadoController {
     }
 
     @GetMapping("/pendentes")
-    public ResponseEntity<List<Certificado>> listarPendentes(){
+    public ResponseEntity<List<Certificado>> listarPendentes() throws RegraDeNegocioException{
         List<Certificado> pendentes = certificadoService.listarCerticadosPendentes(null);
         return ResponseEntity.ok(pendentes);
     }
 
     @GetMapping("/assinados")
-    public ResponseEntity<List<Certificado>> listarAssinados(){
+    public ResponseEntity<List<Certificado>> listarAssinados() throws RegraDeNegocioException{
         List<Certificado> assinados = certificadoService.listarCerticadosAssinados(null);
         return ResponseEntity.ok(assinados);
     }
 
     @GetMapping("/validar/{codigo}")
-    public ResponseEntity<?> validarCertificado(@PathVariable String codigo){
+    public ResponseEntity<?> validarCertificado(@PathVariable String codigo) throws RegraDeNegocioException{
         try {
             Certificado certificadoValido = certificadoService.validarCertificado(codigo);
             return ResponseEntity.ok(certificadoValido);
